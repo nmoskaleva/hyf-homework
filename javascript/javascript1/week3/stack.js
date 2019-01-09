14adefdb702c791abad8ba7ef03b6409a954c38f
nmoskaleva <nsmoskaleva@gmail.com>
2019-01-09 19:35:31 +0100

    added stack.js

--- javascript/javascript1/week3/stack.js
+++ javascript/javascript1/week3/stack.js
@@ -1,33 +1,53 @@
-const watchFirstEpisode = function() {
-    // draw call stack
-    console.log('Watch first episode');
-  };
-  
-  const watchBreakingBad = function() {
-    console.log('Watching Breaking bad');
-    // draw call stack
-    watchFirstEpisode();
-    console.log('No more for today');
-  };
-  
-  const goForARun = function() {
-    console.log('Im running! look at me!');
-  }
-  
-  const helpStudentsWithHomework = function() {
-    console.log('Help help help');
-  }
-  
-  function finishMondayEvening(functionThatIsParameter) {
-    functionThatIsParameter();
-    // draw call stack
-    goForARun();
-    // draw call stack
-    helpStudentsWithHomework();
-    // draw call stack
-    console.log('done for today')
-  }
-  
-  // draw call stack
-  finishMondayEvening(watchBreakingBad);
-  // draw call stack
+const watchFirstEpisode = function () {
+  //watchFirstEpisode()
+  //watchBreakingBad()
+  //finishMondayEvening(watchBreakingBad)
+  console.log('Watch first episode');
+};
+
+const watchBreakingBad = function () {
+  console.log('Watching Breaking bad');
+  //watchBreakingBad();
+  //finishMondayEvening(watchBreakingBad)
+  watchFirstEpisode();
+  console.log('No more for today');
+};
+
+const goForARun = function () {
+  console.log('Im running! look at me!');
+}
+
+const helpStudentsWithHomework = function () {
+  console.log('Help help help');
+}
+
+function finishMondayEvening(functionThatIsParameter) {
+  functionThatIsParameter();
+  //watchBreakingBad();
+  //finishMondayEvening(watchBreakingBad);
+
+  goForARun();
+  //goForARun()
+  //finishMondayEvening(watchBreakingBad)
+
+  helpStudentsWithHomework();
+  //helpStudentsWithHomework();
+  //finishMondayEvening(watchBreakingBad)
+
+  console.log('done for today')
+}
+// draw call stack: now call stack is empty
+
+finishMondayEvening(watchBreakingBad);
+// draw call stack 
+//finishMondayEvening(watchBreakingBad)
+
+/*
+console(logs)
+Watching Breaking bad
+Watch first episode
+No more for today
+Im running! look at me!
+Help help help
+done for today
+*/
