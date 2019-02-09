2ad7fcac41134e14b09efae8156a4fc04f5944ca
nmoskaleva <nsmoskaleva@gmail.com>
2019-02-09 12:59:47 +0100

    homework week 8

--- javascript/javascript3/week2/warmup:movies:rewrite/main.js
+++ javascript/javascript3/week2/warmup:movies:rewrite/main.js
@@ -1,86 +1,14 @@
-// // // // fetch(`https://yesno.wtf/api`)
-// // // // .then(response => response.json())
-// // // // .then((result) => {
-// // // //     console.log(result.answer);
-// // // // })
+//this is homework
+//1 Warmup
+function firstFunction(millisecondsToResolve) {
+    return new Promise(function (resolve) {
+        setTimeout(() => {
+            //console.log(`I am called asynchronously`);
+        }, millisecondsToResolve);
+    })
+}
 
-
-// // // ///battery
-
-// // // let batteryPromise = navigator.getBattery();
-
-// // // let batteryIsCharging = false;
-
-// // // navigator.getBattery().then(function(battery) {
-// // //   batteryIsCharging = battery.charging;
-// // //   console.log(batteryIsCharging);
-// // //   console.log(battery.level * 100);
-// // //   console.log(battery);
-// // // });
-
-// // // // //Create a promise that resolves with a message after 4 seconds. Use this promise by logging out 'hello' after 4 seconds
-// // // // let fourSecondsPromise = new Promise(function(resolve){
-// // // //     setTimeout(function(){
-// // // //         resolve("hello");
-// // // //     }, 4000);
-// // // // });
-
-// // // // fourSecondsPromise.then(function(message){
-// // // //     console.log(message);
-// // // // });
-
-// // // // let twoSecondsPromise = new Promise((resolve) => {
-// // // //     setTimeout(() => resolve(alert("hello")), 2000
-// // // //     );
-// // // // })
-
-// // // // twoSecondsPromise.then(function(){
-
-// // // // });
-
-// // // ///Excercise 4
-// // // //4.1 Get battery level
-// // // navigator.getBattery().then(function(battery){
-// // //     console.log(battery.level * 100 + "%") ;
-// // // })
-// // // //4.2 After the battery level has been gotten, fetch the movies
-// // // .then(() => {
-// // //     return fetch(`https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json`)
-// // // })
-
-// // // //4.3 Log out the movies
-// // // .then((response) => response.json())
-// // // .then((result) => console.log(result));
-
-
-
-// // //4
-// // // let promiseOne = navigator.getBattery();
-// // // let promiseTwo = fetch(`https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json`);
-
-// // // Promise.all([promiseOne, promiseTwo]).then((data) => {console.log(data[1].json.then((movie) => {console.log(movie.type)}))})
-
-
-// // // //****//
-// // // applyForVisa() //it's a promise
-// // // .then(bookHotel)
-// // // .then(buyTickets)
-// // // .catch(cancelVacation);
-
-// // // applyForVisa()
-// // // .then(bookHotel, cancelVacation);
-
-// // //this is homework
-// // //1 Warmup
-// // function firstFunction(millisecondsToResolve) {
-// //     return new Promise(function (resolve) {
-// //         setTimeout(() => {
-// //             //console.log(`I am called asynchronously`);
-// //         }, millisecondsToResolve);
-// //     })
-// // }
-
-// firstFunction(6000);
+firstFunction(6000);
 
 //2. Movies
 //without chaining
@@ -155,14 +83,14 @@ fetchMovies()
     })
 
 
-// //Fetching and waiting
-// fetch(`https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json`)
-// .then(response => response.json())
-// .then(movies => {
-//     setTimeout(() => {
-//         console.log(movies);
-//     }, 3000);
-// })// is it using chaining?
+//Fetching and waiting
+fetch(`https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json`)
+.then(response => response.json())
+.then(movies => {
+    setTimeout(() => {
+        console.log(movies);
+    }, 3000);
+})// is it using chaining?
 
 
 //Rewrite timeout
@@ -195,47 +123,3 @@ getCurrentLocation()
         // called if there was an error getting the users location
         console.log(error);
     });
-
-//*** */
-
-const promise = new Promise((resolve) => {
-    setTimeout(() => {
-        const tea = {
-            color: 'green',
-            taste: 'Bitter',
-        };
-
-        resolve(tea);
-    }, 3000);
-});
-
-const isThereMoreTea = true;
-
-function makeTea() {
-    console.log('Start making tea');
-
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const tea = {
-                color: 'green',
-                taste: 'Bitter',
-            };
-
-            if (isThereMoreTea) {
-                resolve(tea);
-            } else {
-                reject('We dont have more TEA!!');
-            }
-        }, 3000);
-    });
-}
-
-console.log(makeTea());
-
-makeTea()
-    .then((returnedTeaObject) => {
-        console.log(returnedTeaObject);
-    })
-    .catch((error) => {
-        console.log(error);
-    })
