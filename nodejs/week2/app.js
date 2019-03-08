96059b1884d71842dd3a612c8718d9208af5d52d
nmoskaleva <nsmoskaleva@gmail.com>
2019-03-08 12:25:21 +0100

    Added homework week2

---
+++ nodejs/week2/app.js
@@ -0,0 +1,54 @@
+var http = require('http');
+var fs = require('fs');
+var StudentBook = require('./studentbook'); //it's a path, . stands for one directory up
+
+var server = http.createServer(function (req, res) {
+    var url = req.url;
+
+    var hyf_students = new StudentBook.StudentBook();
+
+    var Natalia = {
+        "name": "Natalia",
+        "classId": 08,
+        "email": "nat@gmail.com",
+        "phone": "(745) 235-6338"
+    }
+
+    hyf_students.addStudent(Natalia);
+    hyf_students.addStudent(Natalia); //adding an existing student, but why 3 times console.log? http://prntscr.com/muz42l
+    hyf_students.editStudentInfo({
+        "name": "Hakki",
+        "classId": 6,
+        "email": "hakki@gmail.com",
+        "phone": "(263) 972-6043"
+    })
+
+    //routing
+    if (url == '/getstudentslist') {
+        res.end(JSON.stringify(hyf_students.getStudentsList()))
+    } else if (url == '/getlistbyclass') {
+        res.end(JSON.stringify(hyf_students.getListByClass(07)))
+    } else if (url == '/getlistbyname') {
+        res.end(JSON.stringify(hyf_students.getStudentDetailByName('Nat')))
+    } else if (url == '/editinfo') {
+        res.end(JSON.stringify(hyf_students.getStudentDetailByName('Hakki')))
+    } else if (url.match(/.css$/)) {
+        var fileStream = fs.createReadStream('./main.css', 'UTF-8');
+        res.writeHead(200, {
+            'Content-Type': 'text/css'
+        });
+        fileStream.pipe(res);
+    } else {
+        fs.readFile('index.html', 'utf-8', function (err, data) {
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            res.write(data);
+            res.end();
+        });
+    }
+});
+
+server.listen(8001, function () {
+    console.log('Your server is running at port 8001...');
+});
---
+++ nodejs/week2/studentbook.js
@@ -0,0 +1,82 @@
+var studentsFullList = require('./hyf-students.json');
+var fs = require ('fs');
+
+class Student {
+    constructor(name, classId, email, phone) {
+        this.name = name;
+        this.classId = classId;
+        this.email = email;
+        this.phone = phone;
+    }
+}
+
+class StudentBook {
+    // constructor() {
+    //     this.studentsList = [];
+    // }
+
+    //1. Get full student list
+    getStudentsList() {
+        let fullListOfStudents = studentsFullList.map(student => {
+            let name = student.name;
+            let email = student.email;
+            return {
+                name,
+                email
+            }
+        });
+        return fullListOfStudents;
+    }
+
+    //2. Get list of students by class
+    getListByClass(classNumber) {
+        let classList = studentsFullList.filter(student => student.classId == classNumber);
+        return classList.map(student => {
+            let name = student.name;
+            let classId = student.classId;
+            return {
+                name,
+                classId
+            };
+        });
+    }
+
+    //3. Get student's detailed information by name
+    getStudentDetailByName(name) {
+        let selectedStudent = studentsFullList.filter(student => student.name.toLowerCase().includes(name.toLowerCase()));
+        if (selectedStudent.length == 0) {
+            console.log(`No result found`);
+        } else {
+            return selectedStudent;
+        };
+    }
+
+    // 4. Add a new student to HYF which receive the below person object as an input and store to existing list. Check for duplication. 
+    addStudent(newStudent) {
+        let foundDuplicates = studentsFullList.filter(student => {
+            return student.name.includes(newStudent.name) == true || student.email.includes(newStudent.email) == true
+        });
+        if (foundDuplicates.length > 0) {
+            console.log(`Student already added`);
+        } else {
+            studentsFullList.push(newStudent);
+        }
+    }
+
+    // 5. Edit existing student information
+    editStudentInfo(editedStudentInfo) {
+        studentsFullList.filter(student => {
+            if (editedStudentInfo.name == student.name) {
+                student.classId = editedStudentInfo.classId;
+                student.email = editedStudentInfo.email;
+                student.phone = editedStudentInfo.phone;
+                return student;
+            } else {
+                console.log('No such student')
+            }
+        })
+    }
+}
+
+module.exports.StudentBook = StudentBook; //you export whatever you want to make available from this file. In this case it's a StudentBook class
+module.exports.Student = Student;
